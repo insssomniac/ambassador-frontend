dc00f610de252fd53852156c8ce75a275d72859f
0xSheff <0xsheff@gmail.com>
2022-03-20 12:53:20 +0200

    login and logout and user model

--- vue-admin/src/components/Nav.vue
+++ vue-admin/src/components/Nav.vue
@@ -1,18 +1,28 @@
 <template>
     <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
         <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
-        <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-        <ul class="navbar-nav px-3">
-            <li class="nav-item text-nowrap">
-                <a class="nav-link" href="#">Sign out</a>
-            </li>
-        </ul>
+
+        <nav class="my-2 my-md-0 mr-md-3">
+            <router-link to="/profile" class="p-2 text-white text-decoration-none">
+                {{user.first_name}}&nbsp;{{user.last_name}}
+            </router-link>
+            <a href="#" class="p-2 text-white text-decoration-none" @click="logout">Sign Out</a>
+        </nav>
     </nav>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-    name: "Nav"
+    name: "Nav",
+    props: ['user'],
+    methods: {
+        async logout() {
+            await axios.post('logout');
+            await this.$router.push('/login');
+        }
+    }
 }
 </script>
 
--- vue-admin/src/views/Layout.vue
+++ vue-admin/src/views/Layout.vue
@@ -1,6 +1,6 @@
 <template>
     <v-app>
-        <Nav/>
+        <Nav :user="user"/>
 
         <div class="container-fluid">
             <div class="row">
@@ -38,9 +38,25 @@
 <script>
 import Nav from "../components/Nav";
 import Menu from "../components/Menu";
+import axios from "axios";
+import {User} from "@/models/user";
+
 export default {
     name: "Layout",
-    components: {Menu, Nav}
+    components: {Menu, Nav},
+    data() {
+        return{
+            user: new User()
+        }
+    },
+    async mounted() {
+        try {
+            const {data} = await axios.get('user');
+            this.user = data;
+        } catch (e) {
+            await this.$router.push('/login');
+        }
+    }
 }
 </script>
 
