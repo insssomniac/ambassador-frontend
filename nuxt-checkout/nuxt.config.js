ef96a50ab5892bddbad4dfc538421a77d8faed95
0xSheff <0xsheff@gmail.com>
2022-03-24 17:10:15 +0200

    added correct version of bootstrap to the config
    and basic checkout html

--- nuxt-checkout/nuxt.config.js
+++ nuxt-checkout/nuxt.config.js
@@ -12,7 +12,9 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css',
+        integrity: 'sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3', crossOrigin: 'anonymous'}
     ]
   },
 
