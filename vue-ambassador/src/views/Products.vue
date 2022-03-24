4064222fe914e076be14b4f3f6d6e72fd7237031
0xSheff <0xsheff@gmail.com>
2022-03-24 16:38:41 +0200

    generating links functionality added

--- vue-ambassador/src/views/Products.vue
+++ vue-ambassador/src/views/Products.vue
@@ -1,17 +1,29 @@
 <template>
-    <div class="mb-4 input-group">
-        <input class="form-control" placeholder="Search" @keyup="search($event.target.value)"/>
-        <div class="input-group-append">
-            <select class="form-select" @change="sort($event.target.value)">
-                <option>Select</option>
-                <option value="asc">Price Ascending</option>
-                <option value="desc">Price Descending</option>
-            </select>
+    <div class="alert alert-info" v-if="link">
+        {{ link }}
+    </div>
+    <div class="alert alert-danger" v-if="error">
+        {{ error }}
+    </div>
+    <div class="row mb-4">
+        <div class="col-md-10 input-group">
+            <input class="form-control" placeholder="Search" @keyup="search($event.target.value)"/>
+            <div class="input-group-append">
+                <select class="form-select" @change="sort($event.target.value)">
+                    <option>Select</option>
+                    <option value="asc">Price Ascending</option>
+                    <option value="desc">Price Descending</option>
+                </select>
+            </div>
+        </div>
+        <div class="col-md-2" v-if="selected.length > 0">
+            <button class="btn btn-info" @click="generateLink">Generate Link</button>
         </div>
     </div>
+
     <div class="row">
-        <div class="col-md-4" v-for="product in products" :key="product.id">
-            <div class="card mb-4 shadow-sm">
+        <div class="col-md-4" v-for="product in products" :key="product.id" @click="select(product.id)">
+            <div class="card mb-4 shadow-sm" :class="selected.some(s => s === product.id) ? 'selected' : ''">
                 <img :src="product.image" height="200" alt="">
                 <div class="card-body">
                     <p class="card-text">{{ product.title }}</p>
@@ -28,13 +40,18 @@
 </template>
 
 <script lang="ts">
-import {SetupContext} from "vue";
+import {ref, SetupContext} from "vue";
+import axios from "axios";
 
 export default {
     name: "Products",
     props: ['products', 'filters', 'lastPage'],
     emits: ['set-filters'],
     setup(props: any, context: SetupContext) {
+        const selected = ref<number[]>([]);
+        const link = ref<string>('');
+        const error = ref<string>('');
+
         const search = (s: string) => {
             context.emit('set-filters', {
                 ...props.filters,
@@ -58,15 +75,53 @@ export default {
             });
         }
 
+        const select = (id: number) => {
+            if (selected.value.some(s => s === id)) {
+                selected.value = selected.value.filter(s => s !== id);
+                return;
+            }
+
+            selected.value = [...selected.value, id];
+        }
+
+        const generateLink = async () => {
+            try {
+                const {data} = await axios.post('links', {
+                    products: selected.value
+                });
+
+                link.value = `Link generated: ${process.env.VUE_APP_CHECKOUT_URL}/${data.code}`;
+            } catch (e) {
+                error.value = 'You should be logged in to generate a link!';
+            } finally {
+                setTimeout( () => {
+                    link.value = '';
+                    error.value = '';
+                }, 5000);
+            }
+
+        }
+
         return {
+            selected,
+            link,
+            error,
             search,
             sort,
-            loadMore
+            loadMore,
+            select,
+            generateLink
         }
     }
 }
 </script>
 
 <style scoped>
+    .card {
+        cursor: pointer;
+    }
 
+    .card.selected {
+        border: 4px solid darkcyan;
+    }
 </style>
